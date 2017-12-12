7d8be8877c49432c7d50fac35a937a538ce416c4
Shaihulud <swaroghh@yandex.ru>
2017-12-12 14:48:43 +0300

    menu done

--- local/templates/theme_typical/header.php
+++ local/templates/theme_typical/header.php
@@ -32,6 +32,26 @@
 </head>
 <body>
 <?$APPLICATION->ShowPanel();?>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:menu", 
+	"horizontal_multilevel", 
+	array(
+		"ALLOW_MULTI_SELECT" => "N",
+		"CHILD_MENU_TYPE" => "left",
+		"DELAY" => "N",
+		"MAX_LEVEL" => "2",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_TYPE" => "N",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"MENU_THEME" => "site",
+		"ROOT_MENU_TYPE" => "catalog",
+		"USE_EXT" => "Y",
+		"COMPONENT_TEMPLATE" => "horizontal_multilevel"
+	),
+	false
+);
 
 
 	
